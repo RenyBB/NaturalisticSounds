0f69f15c25c21da088c058e5a6c6c017279e7b6b
RenyBB <71499318+RenyBB@users.noreply.github.com>
2022-05-27 13:30:59 +0100

    Add files via upload

---
+++ NaturalisticSounds/7650/Stimuli.js
@@ -0,0 +1,23 @@
+var NaturalisticSounds_7650 = [
+    {
+        "Stimulus": "99496__julien-matthey__soccer-supporters-in-the-street_7650.mp3"
+    },
+    {
+        "Stimulus": "181955__qubodup__felix-blume-cows-mooing-1-min_7650.mp3"
+    },
+    {
+        "Stimulus": "339517__pulswelle__waves-at-baltic-sea-shore_7650.mp3"
+    },
+    {
+        "Stimulus": "393353__nsdap__laughing_7650.mp3"
+    },
+    {
+        "Stimulus": "541595__breviceps__german-ambulance-2_7650.mp3"
+    },
+    {
+        "Stimulus": "565309__animadierer__lion-roar-snarl-growl-at013601_7650.mp3"
+    },
+    {
+        "Stimulus": "623134__brsjak__wasps_7650.mp3"
+    }
+]
